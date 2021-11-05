258147d355090828a9e078b343fb0b47a941118d
Ufere Kalu <dekalusha@gmail.com>
2021-11-05 09:59:09 -0600

    add logo and functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./images/side-huzzle-img.png";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (event) => {
+    console.log(event.targe.value);
+  };
+
   return (
     <div className="App">
-      Hello World!
+      <div className="content-holder">
+        {input ? (
+          input
+        ) : (
+          <img
+            style={{ height: "100px", width: "400px" }}
+            src={logo}
+            alt="side huzzle logo"
+          />
+        )}
+      </div>
+      <div className="text-input">
+        <input
+          value={input}
+          name="input"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
